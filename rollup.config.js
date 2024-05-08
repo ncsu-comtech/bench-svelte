6df93334d64e5230140e469b1fe1138618a1ff6f
Matt Haught <dmhaught@ncsu.edu>
2024-05-08 01:33:06 -0400

    update dev settings

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,23 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from 'rollup-plugin-node-resolve';
+import svelte from "rollup-plugin-svelte";
+import resolve from "rollup-plugin-node-resolve";
 
-const pkg = require('./package.json');
+const pkg = require("./package.json");
 
 export default {
-    input: [
-        'src/index.js'
-    ],
-    output: [
-        {file: pkg.module, format: 'es', name: 'Bench'},
-        {file: pkg.main, format: 'umd', name: 'Bench'},
-    ],
-    plugins: [
-        svelte({
-            customElement: true,
-            tag: "bench",
-            emitCss: true
-        }),
-        resolve({
-                extensions: ['.svelte', '.mjs', '.js', '.jsx', '.json'],
-                mainFields: ['jsnext:main', 'module', 'main']
-            }
-        )
-    ]
+  input: ["src/index.js"],
+  output: [
+    { file: pkg.module, format: "es", name: "Bench" },
+    { file: pkg.main, format: "umd", name: "Bench" },
+  ],
+  plugins: [
+    svelte({
+      customElement: true,
+      tag: "bench",
+      emitCss: true,
+    }),
+    resolve({
+      extensions: [".svelte", ".mjs", ".js", ".jsx", ".json"],
+      mainFields: ["jsnext:main", "module", "main"],
+    }),
+  ],
 };
